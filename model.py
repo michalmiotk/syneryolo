47a0f1ee8ee85541939523f13416b1f4a8a6e7e0
michalmiotk <michalmiotk95@gmail.com>
2021-01-06 18:46:08 +0100

    code rewritten for pytorchlightning, added wandbai

--- model.py
+++ model.py
@@ -4,46 +4,72 @@ import torchvision.models as models
 import torchvision
 from my_transforms import Transform_img_labels
 from utils import Utils
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import WandbLogger
+import json
+import os
 
-class Yolo(nn.Module):
+with open("api_key.txt", "r") as f:
+    api_key = json.load(f)
+    print("opened")
+os.environ["WANDB_API_KEY"] = api_key["api_key"]
+
+
+class Yolo(pl.LightningModule):
     def __init__(self):
         super().__init__()
         self.model = models.vgg16(pretrained=True)
         self.prelast_linear = torch.nn.Linear(512*7*7, 4096)
         self.last_linear = torch.nn.Linear(4096, 7*7*30)
         self.last_relu = torch.nn.ReLU()
-        self.lambda_coord = torch.Tensor([0.5])
-        self.lambda_noobj = torch.Tensor([0.5])
-        self.cats = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
-                     'dog', 'horse', 'motorbike', 'person', 'sheep', 'sofa', 'diningtable', 'pottedplant', 'train', 'tvmonitor']
+        self.lambda_coord = torch.Tensor([0.5]).cuda()
+        self.lambda_noobj = torch.Tensor([0.5]).cuda()
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.00001)
+        return optimizer
 
     def get_index(self, name):
         return self.cats.index(name)
-
-    def dataset_inspect(self):
-        optimizer = torch.optim.RMSprop(self.parameters(), lr=0.00001)
-        root_dir = "/home/m/Pobrane/VOC2007"
-        transformacje=Transform_img_labels()
-        self.dataset = torchvision.datasets.VOCDetection(
-            root_dir, year="2007", image_set='train', download=True, transforms=transformacje)
-        for epoch_nr in range(10):
-            for i, x in enumerate(self.dataset):
-                image, annot = x
-                image = image.unsqueeze(0)
-                
-                out = self.forward(image)
-                loss1 = self.get_loc_error(out, annot)
-                loss2 = self.get_w_h_error(out, annot)
-                loss3 = self.get_conditional_class_prob_exist(out, annot)
-                loss4 = self.get_conditional_class_prob_notexists(out, annot)
-                loss5 = self.get_confidence_error(out, annot)
-                loss = loss1 + loss2+loss3+loss4+loss5
-                self.zero_grad()
-                loss.backward()
-                optimizer.step()
-                if i%100 == 0:
-                    print(epoch_nr,i, loss)
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch
         
+        if len(y) == 2:
+            img, annot = y
+        else:
+            img = y
+            annot = {}
+        outputs = self.forward(x)
+        loss = self.loss_fn(outputs, annot)
+        self.log('train_loss', loss, on_step=True, on_epoch=False)
+        return {'loss': loss}
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        annot = y
+        outputs = self.forward(x)
+        loss = self.loss_fn(outputs, annot)
+        self.log('val_loss', loss, on_step=True, on_epoch=False)
+        return {'loss': loss}
+    
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        img, annot = y
+        outputs = self.forward(x)
+        loss = self.loss_fn(outputs, annot)
+
+        
+    
+    def loss_fn(self, out, annot):
+        loss1 = self.get_loc_error(out, annot)
+        loss2 = self.get_w_h_error(out, annot)
+        loss3 = self.get_conditional_class_prob_exist(out, annot)
+        loss4 = self.get_conditional_class_prob_notexists(out, annot)
+        loss5 = self.get_confidence_error(out, annot)
+        return loss1 + loss2+loss3+loss4+loss5
+    
     def some_iterator(self, predictions):
         predictions_view = predictions.view(-1, 7,7, 30)
         for image_nr in range(predictions_view.shape[0]):
@@ -61,13 +87,15 @@ class Yolo(nn.Module):
             x1,y1,_,_,_,x2,y2 = pred[:7]
             if (y,x) in targets:
                 for i, tar in enumerate(targets[(y,x)]):
-                    ypred, xpred = tar[2]
+                   
+                    ypred, xpred = tar[2][0], tar[2][1]
                     if i == 0:
                         loss += self.lambda_coord*((xpred-x1)*(xpred-x1)+ (ypred-y1)*(ypred-y1))          
                     elif i == 1:
                         loss += self.lambda_coord*((xpred-x2)*(xpred-x2)+ (ypred-y2)*(ypred-y2))
                     else:
                         break
+                    
         return loss
     
     def get_loc_err_equation(self, wtar, wpred, htar, hpred):
@@ -83,7 +111,6 @@ class Yolo(nn.Module):
                 
                 for i, tar in enumerate(targets[(y,x)]):
                     wpred, hpred = tar[1]
-                    wpred, hpred = torch.Tensor([wpred]),torch.Tensor([hpred]) 
                     if i == 0:
                         loss += self.get_loc_err_equation(w1, wpred, h1, hpred)      
                     elif i == 1:
@@ -98,8 +125,8 @@ class Yolo(nn.Module):
             x1,y1,w1,h1,p1,x2,y2,w2,h2,p2= pred[:10]
             if (y,x) in targets:
                 for i, tar in enumerate(targets[(y,x)]):
-                    tarw, tarh = torch.Tensor(tar[1])
-                    tary, tarx = torch.Tensor(tar[2])
+                    tarw, tarh = torch.Tensor(tar[1]).cuda()
+                    tary, tarx = torch.Tensor(tar[2]).cuda()
                     if i==0:
                         iou = Utils.get_iou(x, y, x1, y1, w1, h1, tarx, tary, tarw, tarh)
                         loss += torch.pow(iou-p1,2)
@@ -119,9 +146,9 @@ class Yolo(nn.Module):
                 des_class = targets[(y,x)][0]
                 for c in range(20):
                     if c == des_class:
-                        loss += torch.pow(torch.Tensor([1])-pred_class[c],2)
+                        loss += torch.pow(torch.Tensor([1]).cuda()-pred_class[c],2)
                     else:
-                        loss += torch.pow(torch.Tensor([0])-pred_class[c],2)                
+                        loss += torch.pow(torch.Tensor([0]).cuda()-pred_class[c],2)                
         return loss
 
     def get_conditional_class_prob_notexists(self, predictions, targets):
@@ -130,7 +157,7 @@ class Yolo(nn.Module):
             pred_class = pred[10:]
             if (y,x) not in targets:
                 for c in range(20):
-                    loss += self.lambda_noobj*torch.pow(torch.Tensor([0])-pred_class[c],2)                
+                    loss += self.lambda_noobj*torch.pow(torch.Tensor([0]).cuda()-pred_class[c],2)                
         return loss
 
     def forward_imagenet(self, input):
@@ -146,8 +173,35 @@ class Yolo(nn.Module):
         x = self.last_linear(x)
         x = self.last_relu(x)
         return x
+    
+class DataModule(pl.LightningDataModule):
+    def __init__(self, bs):
+        super().__init__()
+        self.batch_size = bs
+        self.transform_VOC = Transform_img_labels()
+        self.root_dir_train = "/home/m/Pobrane/VOC2007train"
+        self.root_dir_val = "/home/m/Pobrane/VOC2007val"
+        self.root_dir_test = "/home/m/Pobrane/VOC2007test"
+
+    def prepare_data(self):
+        self.train_dataset = torchvision.datasets.VOCDetection(self.root_dir_train, year='2007',image_set='train', download=True, transforms=self.transform_VOC)
+        self.trainval_dataset = torchvision.datasets.VOCDetection(self.root_dir_val, year='2007',image_set='trainval', download=True, transforms=self.transform_VOC)
+        self.val_dataset = torchvision.datasets.VOCDetection(self.root_dir_test, year='2007', image_set='val', download=True, transforms=self.transform_VOC)
+    # Creating train batches
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(dataset=self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=3, pin_memory=True)
 
 
-yolo = Yolo()
-yolo.dataset_inspect()
-#yolo.forward(torch.Tensor(2,3, 224, 224))
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(dataset=self.trainval_dataset, batch_size=self.batch_size, shuffle=False, num_workers=3, pin_memory=True)
+
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(dataset=self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=3, pin_memory=True)
+
+model = Yolo()
+dm = DataModule(bs=1)
+dm.prepare_data()
+
+wandb_logger = WandbLogger(project="yolo", name='first trials')
+trainer = Trainer(max_epochs=11, fast_dev_run=False, gpus=1, profiler=False, progress_bar_refresh_rate=1, logger=wandb_logger, log_every_n_steps=1)
+trainer.fit(model, dm)
